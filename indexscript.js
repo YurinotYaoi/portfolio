a4c5e4fde3fffc62c304a62bd69ca47a9fd2a0ef
Angelo Uriel Solamo <yuri08182004@gmail.com>
2025-04-08 13:17:47 +0800

    js

--- indexscript.js
+++ indexscript.js
@@ -1,5 +1,7 @@
 const openButton = document.getElementById('open-sidebar-button')
 const navbar = document.getElementById('navbar')
+const avadiv = document.getElementById('avatardescriptiondiv')
+const img = document.getElementById('imgdiv')
 
 const media=window.matchMedia("(width <= 860px)")
 
@@ -12,8 +14,17 @@ function updateNavbar(e){
 
     if(isMobile) {
         navbar.setAttribute('inert','')
+        avadiv.classList.remove('animate__bounceInRight')
+        avadiv.classList.add('animate__backInUp')
+        img.classList.remove('animate__fadeInLeft')
+        img.classList.add('animate__backInDown')
+
     } else {
         navbar.removeAttribute('inert')
+        avadiv.classList.remove('animate__backInUp')
+        avadiv.classList.add('animate__bounceInRight')
+        img.classList.remove('animate__backInDown')
+        img.classList.add('animate__fadeInLeft')
     }
 }
 
